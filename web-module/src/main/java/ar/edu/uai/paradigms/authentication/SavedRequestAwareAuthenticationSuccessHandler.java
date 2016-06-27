0d1a54279be1bbca7e6aad5caafdac4ef05f239b
Ramiro Fages <fages_fages@hotmail.com>
2016-06-26 21:33:28 -0300

    Ahora se retorna el permiso del usuario logueado

--- web-module/src/main/java/ar/edu/uai/paradigms/authentication/SavedRequestAwareAuthenticationSuccessHandler.java
+++ web-module/src/main/java/ar/edu/uai/paradigms/authentication/SavedRequestAwareAuthenticationSuccessHandler.java
@@ -1,6 +1,9 @@
 package ar.edu.uai.paradigms.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
@@ -8,9 +11,11 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Federico on 24/03/2015.
@@ -19,10 +24,19 @@ public class SavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuth
 
     private RequestCache requestCache = new HttpSessionRequestCache();
 
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(SimpleAuthenticationProvider.class);
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws ServletException, IOException {
         SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        List<GrantedAuthority> permisos = (List<GrantedAuthority>)authentication.getAuthorities();
+
+        Cookie credenciales = new Cookie("credenciales",permisos.get(0).getAuthority());
+        response.addCookie(credenciales);
 
         if (savedRequest == null) {
             clearAuthenticationAttributes(request);
